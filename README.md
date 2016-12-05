716290d178100dcaff75c5c43827075d102f5f1e
Anthony Gore <anthony@magnum.technology>
2016-12-05 11:40:34 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Vue.js Essentials: Build Your First Vue App
+
+### Vue.js Poster Store
+
+#### Pre-installation
+
+1. Ensure that npm is installed globally
+2. Register Oauth 2 access to the [Imgur API](https://api.imgur.com/oauth2/addclient).
+
+    Register for OAuth 2 authorization without a callback URL. You can name your application anything you like and you don't need a callback URL. The important thing is that you get a *client ID*. 
+
+#### Installation
+
+1. Clone this repository on your local file system
+
+    ```
+    cd /path/to/install/location
+    git clone git@github.com:getjsdojo/vuejs-poster-shop.git
+    ```
+
+2. Install dependencies
+
+    ```
+    npm install
+    ```
+
+3. Create a `.env` file by copying the sample
+
+    ```
+    cp .env_sample .env
+    ```
+    
+    Now edit the .env file and replace the `IMGUR_CLIENT_ID` with the client ID provided in the pre-installation
+    
+4. Start project
+
+    ```
+    npm run start
+    ```
+
+5. Your site will be available at *localhost:[PORT]* where `PORT` is whatever value is set in your `.env` file.
---
+++ index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var reload = require('reload');
+var axios = require('axios');
+var querystring = require('querystring');
+
+require('dotenv').config();
+
+var bodyParser = require('body-parser');
+app.use( bodyParser.json() );
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+var instance = axios.create({
+  baseURL: 'https://api.imgur.com/3/',
+  headers: { 'Authorization': 'Client-ID ' + process.env.IMGUR_CLIENT_ID }
+});
+
+app.get('/search/:query', function(req, res) {
+  const url = 'gallery/search/top/0/?' + querystring.stringify({ q: req.params.query });
+  instance.get(url)
+    .then(function (result) {
+      res.send(result.data.data.filter(item => !item.is_album && !item.nsfw && !item.animated));
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  ;
+});
+
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+reload(server, app);
+
+server.listen(process.env.PORT, function () {
+  console.log('Listening on port '.concat(process.env.PORT))
+});
