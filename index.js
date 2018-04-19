1d1f6ca845bb6ec80ca4ea48732c8bef2dec00d2
Anthony Gore <anthony@magnum.technology>
2018-04-19 15:09:16 +0700

    Adding pusher to server

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ var path = require('path');
 var server = require('http').createServer(app);
 var axios = require('axios');
 var querystring = require('querystring');
+var Pusher = require('pusher');
 
 require('dotenv').config();
+
+var pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: process.env.PUSHER_CLUSTER
+});
 
 var bodyParser = require('body-parser');
 app.use( bodyParser.json() );
@@ -29,6 +37,10 @@ app.get('/search/:query', function(req, res) {
       console.log(error);
     })
   ;
+});
+
+app.post('/cart_update', function(req, res) {
+  pusher.trigger('cart', 'update', req.body);
 });
 
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
